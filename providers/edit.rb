16879f6dcbaf1356b752d56c8eec789e72c7e320
Erik Hollensbe <erik+github@hollensbe.org>
2012-04-26 23:51:23 -0700

    if the file doesn't exist, return an empty set.

--- providers/edit.rb
+++ providers/edit.rb
@@ -52,6 +52,8 @@ end
 def parse_known_hosts(filename)
   keys = { }
 
+  return keys unless File.exist?(filename)
+
   ::File.read(::File.expand_path(filename)).each_line.with_index do |line, index|
     info, key = line.split(" ", 2)
     hostname, ipaddr = info.split(",", 2)
