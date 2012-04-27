1d8604ee49a5ae9d7d3b87d0a4fdab7df10637d0
Erik Hollensbe <erik+github@hollensbe.org>
2012-04-27 00:05:08 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "erik+chef@hollensbe.org"
 license           "Apache 2.0"
 description       "backups with hardlink support with rsync and ssh."
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.1.1"
 
 %w{ debian ubuntu }.each do |os|
   supports os
